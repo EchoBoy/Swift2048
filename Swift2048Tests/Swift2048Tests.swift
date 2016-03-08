681c6d3d44c391c2278fe1ea1e0a6b4854b3bb17
EchoBoy <861278197@qq.com>
2016-03-08 14:45:09 +0800

    Initial Commit

---
+++ Swift2048Tests/Swift2048Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift2048Tests.swift
+//  Swift2048Tests
+//
+//  Created by 李航 on 3/8/16.
+//  Copyright © 2016 ThrEcho. All rights reserved.
+//
+
+import XCTest
+@testable import Swift2048
+
+class Swift2048Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
